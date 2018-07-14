4325dbefc9e01ba458f843079747af1239ba42c7
Agata <twardowska123@gmail.com>
2018-07-14 17:35:47 +0200

    poprawki

--- src/Lines.java
+++ src/Lines.java
@@ -1,18 +1,39 @@
 public class Lines {
 
-    Line line1 = new Line(4,5,4,7);
-
-    public double distance1() {
-        return Math.sqrt((line1.p1.x - line2.p2.x) * (line1.p1.x - line2.p2.x) + (line1.p1.y - line2.p2.y) * (line1.p1.y - line2.p2.y));
-    }
-
-    Line line2 = new Line(2,6,2,11);
-
-    public double distance2() {
-        return Math.sqrt((line1.p1.x - line2.p2.x) * (line1.p1.x - line2.p2.x) + (line1.p1.y - line2.p2.y) * (line1.p1.y - line2.p2.y));
+    public double distance(Line line) {
+        double distance;
+        return Math.sqrt((line.p1.x - line.p2.x) * (line.p1.x - line.p2.x) + (line.p1.y - line.p2.y) * (line.p1.y - line.p2.y));
 
     }
 
+    String longerLine(Line line1, Line line2) {
+
+        if (distance(line1) > distance(line2)) {
+            System.out.println("Linia 1 jest dłuższa");
+            System.out.println("Współrzędne początku to:" + " " + line1.p1.x + ',' + line1.p1.y);
+            System.out.println("Współrzędne końca to:" + " " + line1.p2.x + ',' + line1.p2.y);
+            System.out.println("Długość lini wynosi:" + " " + distance(line1));
+        } else {
+            System.out.println("Linia 2 jest dłuższa");
+            System.out.println("Współrzędne początku to:" + " " + line2.p1.x + ',' + line2.p1.y);
+            System.out.println("Współrzędne końca to:" + " " + line2.p2.x + ',' + line2.p2.y);
+            System.out.println("Długość lini wynosi:" + " " + distance(line2));
+
+        }
+
+        return null;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
--- src/Test.java
+++ src/Test.java
@@ -14,7 +14,7 @@ public class Test {
         int x2 = input.nextInt();
         int y2 = input.nextInt();
 
-        System.out.println("Wprowadź współrzęde punktu p1 lini 2:");
+        System.out.println("Wprowadź współrzędne punktu p1 lini 2:");
         int a1 = input.nextInt();
         int b1 = input.nextInt();
         System.out.println("Wprowadź współrzędne punktu p2 lini 2:");
@@ -23,7 +23,14 @@ public class Test {
 
         Line line1 = new Line(x1, y1, x2, y2);
         Line line2 = new Line(a1, b1, a2, b2);
+
+        Lines lines = new Lines();
+        double dlugoscLine1 = lines.distance(line1);
+        double dlugoscLine2 = lines.distance(line2);
+
+        String longer = lines.longerLine(line1, line2);
+
+
     }
+
 }
-
-
